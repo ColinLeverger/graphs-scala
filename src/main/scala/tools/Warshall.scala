55044df2967083962ead1cec56964159cbcbfcbb
Colin LEVERGER <colinleverger@gmail.com>
2015-11-22 11:54:45 +0100

    routing

--- src/main/scala/tools/Warshall.scala
+++ src/main/scala/tools/Warshall.scala
@@ -1,6 +1,7 @@
 package tools
 
 import graph._
+import scala.collection.mutable.{Map => MMap}
 
 /**
   * Created by colinleverger on 30/10/15.
@@ -12,19 +13,25 @@ object Warshall {
     * @param graph
     * @return nothing, it modify the graph itself!
     */
-  def applyWarshall(graph: Graph) {
+  def applyWarshall(graph: Graph): MMap[Map[Int, Int], Int] = {
     val n = graph.nbNodes
+    val routing = MMap[Map[Int, Int], Int]()
 
     for {
       i <- 1 to n
       x <- 1 to n
       y <- 1 to n
     } {
-      if (graph.isArc(x, i) && graph.isArc(i, y) && !graph.isArc(x, y)) {
-        //println("Adding an arc from node " + x + " to node " + y) //DEBUG
-        graph.getNode(x).addArc(y, 1)
+      if (graph.isArc(x, i)) {
+        routing(Map(x -> i)) = x
+        if (graph.isArc(i, y) && !graph.isArc(x, y)) {
+          //println("Adding an arc from node " + x + " to node " + y) //DEBUG
+          graph.getNode(x).addArc(y, 1)
+          routing(Map(x -> y)) = i
+        }
       }
     }
+    routing
   }
 }
 
